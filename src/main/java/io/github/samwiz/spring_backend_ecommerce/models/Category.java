812ba01c3f50d6e0bccfd1b67edb79627370853a
Sam-wiz <samrudhvvandakudri2005@gmail.com>
2024-04-12 10:24:52 +0530

    Implementing API's lecture done

--- src/main/java/io/github/samwiz/spring_backend_ecommerce/models/Category.java
+++ src/main/java/io/github/samwiz/spring_backend_ecommerce/models/Category.java
@@ -1,4 +1,13 @@
 package io.github.samwiz.spring_backend_ecommerce.models;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
-public class Category {
+@Getter
+@AllArgsConstructor
+public class Category
+{
+    private Long id;
+    private String title;
+    private String description;
 }
--- src/main/java/io/github/samwiz/spring_backend_ecommerce/models/Product.java
+++ src/main/java/io/github/samwiz/spring_backend_ecommerce/models/Product.java
@@ -1,6 +1,18 @@
 package io.github.samwiz.spring_backend_ecommerce.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Product
 {
-
+    private Long id;
+    private String title;
+    private String description;
+    private Double price;
+    private Category category;
+    private String image;
 }
