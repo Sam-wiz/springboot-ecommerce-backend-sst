8abb480be69579f5c40904e3fc9012163f922362
Sam-wiz <samrudhvvandakudri2005@gmail.com>
2024-04-14 06:07:17 +0530

    Completed class Intro to ORMs, Models, Representing Cardinalities

--- src/main/java/io/github/samwiz/spring_backend_ecommerce/models/Category.java
+++ src/main/java/io/github/samwiz/spring_backend_ecommerce/models/Category.java
@@ -1,12 +1,16 @@
 package io.github.samwiz.spring_backend_ecommerce.models;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Category
 {
+    @Id
     private Long id;
     private String title;
     private String description;
